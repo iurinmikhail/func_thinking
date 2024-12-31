7e93655b36615c95ef33b56f75b5de45e6b23efc
iuma <reggavana56@mail.ru>
2024-12-31 14:57:17 +0700

    feat: Добавлена функция для прямолинейной реализации

--- shopping/mail.py
+++ shopping/mail.py
@@ -2,22 +2,29 @@ from typing import TypedDict, NamedTuple, TypeAlias, Any
 
 
 MAIL_LIST = []
+
+
 class Email(NamedTuple):
     value: str
+
 
 class Elements(TypedDict):
     email: Email
 
+
 class Form(NamedTuple):
     elements: Elements
 
+
 class Event(NamedTuple):
     target: Form
+
 
 def add(array: list, elem: Any) -> list:
     new_arr = array.copy()
     new_arr.append(elem)
     return new_arr
+
 
 def add_contact(mail_list: list[Email], email: Email) -> list[Email]:
 
@@ -30,8 +37,9 @@ def submit_form_handler(event: Event) -> None:
     email = form.elements["email"].value
     MAIL_LIST = add_contact(MAIL_LIST, email)
 
+
 if __name__ == "__main__":
-    email = Email(value='123@mail.com')
+    email = Email(value="123@mail.com")
     event = Event(target=Form(elements=Elements(email=email)))
     submit_form_handler(event=event)
     assert MAIL_LIST == [email.value]
